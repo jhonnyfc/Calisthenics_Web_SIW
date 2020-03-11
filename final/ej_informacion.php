4f02fecc6581793bd2ece935a8e700f40f7e2776
danieldbg1 <49949803+danieldbg1@users.noreply.github.com>
2020-03-11 11:40:52 +0100

    Add files via upload

--- final/ej_informacion.php
+++ final/ej_informacion.php
@@ -1,109 +1,81 @@
 <?php 
 
-	$conexion = mysqli_connect("localhost", "root", "", "mysql");
+	$conexion = mysqli_connect("localhost", "root", "", "grupo33");
 
 	if (!$conexion) {
 		die("Conexión fallida: " . mysqli_connect_error());
 	}
-	//echo "Conexión creada correctamente<br>";
-
-
-
 
 	$consulta = "select * from final_publicacion";
-/*
-	if ($resultado = $conexion->query($consulta)) {
-		print_r("OK<br>");
-	} else {
-		print_r("mal<br>");
-	}
-
-	echo "<br>";
-
-	if ($resultado->num_rows > 0) {
-		// sacamos los datos para cada registro
-		while($fila = $resultado->fetch_assoc()) {
-			echo "IDPUBLICACION: " . $fila["IDPUBLICACION"]. " - FECHA_PUBLICACION: " .
-			$fila["FECHA_PUBLICACION"]. "- CONTENIDO: " . $fila["CONTENIDO"]."- AUTOR: ".$fila["AUTOR"]. "<br>";
-		}
-	} else {
-		echo "No hay resultados";
-	}
-*/
 	if ($resultado = $conexion->query($consulta)) {
 		//print_r("OK<br>");
 	} else {
-		//print_r("mal<br>");
+		echo "Error en la query";
+		die(-1);
 	}
 
 	$valores = array("IDPUBLICACION", "FECHA_PUBLICACION", "CONTENIDO", "AUTOR");
 	$cont=0;
 
-	/*$miarray = array("titulo"=>"Las legiones malditas",
-						  "autor" => "Santiago Posteguillo",
-						  "editorial" => "Ediciones B",
-						  "fechaPublicacion" => 2008);
-  */
 
 	if ($resultado->num_rows > 0) {
 		// sacamos los datos para cada registro
 		while($fila = $resultado->fetch_assoc()) {
-			//$valores[$cont] = $fila["IDPUBLICACION"]."-".$fila["FECHA_PUBLICACION"]."-".$fila["CONTENIDO"]."-".$fila["AUTOR"];
 			$valores[$cont] = array("IDPUBLICACION"=>$fila["IDPUBLICACION"],
+							"TITULO"=>$fila["TITULO"], 
 							 "FECHA_PUBLICACION"=>$fila["FECHA_PUBLICACION"], 
 							 "CONTENIDO"=>$fila["CONTENIDO"], 
 							 "AUTOR"=>$fila["AUTOR"]);
 			$cont++;
-			//echo "IDPUBLICACION: " . $fila["IDPUBLICACION"]. " - FECHA_PUBLICACION: " .$fila["FECHA_PUBLICACION"]. "- CONTENIDO: " . $fila["CONTENIDO"]."- AUTOR: ".$fila["AUTOR"]. "<br>";
 		}
 	} else {
 		echo "No hay resultados";
 	}
-	/*
-	foreach ($valores as $key => $value) {
-		print_r($value['IDPUBLICACION']."<br>");
-		print_r($value["FECHA_PUBLICACION"]."<br>");
-		print_r($value["CONTENIDO"]."<br>");
-		print_r($value["AUTOR"]."<br>");
-	}
-	*/
-
-	$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
-	$indice = $pagina * 2;
-
 	
-	$longitud_valores = count($valores);
-	$numero_pagina = $longitud_valores/2;//numeros de paginacion(me dan numeros decimales!!!!!! usar intdiv?)
-
 	$fichero=file_get_contents("informacion.html");
 	$trozos=explode("##tarjetaInfo##", $fichero);
 	$carta="";
 
-	print_r( "Numero:pagina: ".$numero_pagina."<br>");
-	print_r( "PAgina: ".$pagina."<br>");
+	$longitud_valores = count($valores);//numero filas de tabla
+	$numero_pagina = $longitud_valores/2;//numeros de paginacion
+	$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+	$indice = $pagina * 2;
 
 
-	if ( ($longitud_valores % 2 == 0) && (($numero_pagina + 1)!=$pagina) ) {//muestro dos tarjetas
+	if ($pagina<$numero_pagina && $pagina==1) {
+		$boton_anterior=$pagina;
+		$boton_siguiente=$pagina+1;
+	}
+	elseif ($pagina>$numero_pagina ) {
+		$boton_anterior=$pagina-1;
+		$boton_siguiente=$pagina;
+	}
+	else{
+		$boton_anterior=$pagina-1;
+		$boton_siguiente=$pagina+1;
+	}
+
+	
+
+
+	if ( ($longitud_valores % 2 == 0) || (($numero_pagina)>=$pagina) ) {//muestro dos tarjetas
+		$aux=$trozos[1];
 		$fila = $valores[$indice-2];
-		$titulo="Primera dominada";
+		$titulo=$fila['TITULO'];
 		$descripcion = $fila['CONTENIDO'];
 		$autor = $fila['AUTOR'];
 		$fecha = $fila['FECHA_PUBLICACION'];
 
-		$fila = $valores[$indice-1];
-		$titulo2="Primera dominada2";
-		$descripcion2 = $fila['CONTENIDO'];
-		$autor2 = $fila['AUTOR'];
-		$fecha2 = $fila['FECHA_PUBLICACION'];
-
-
-		
-		$aux=$trozos[1];
 		$aux=str_replace("##resena_titulo1##", $titulo, $aux);
 		$aux=str_replace("##resena_texto1##", $descripcion, $aux);
 		$aux=str_replace("##resena_autor1##", $autor, $aux);
 		$aux=str_replace("##resena_fecha1##", $fecha, $aux);
-
+		
+		$fila = $valores[$indice-1];
+		$titulo2=$fila['TITULO'];
+		$descripcion2 = $fila['CONTENIDO'];
+		$autor2 = $fila['AUTOR'];
+		$fecha2 = $fila['FECHA_PUBLICACION'];
 
 		$aux=str_replace("##resena_titulo2##", $titulo2, $aux);
 		$aux=str_replace("##resena_texto2##", $descripcion2, $aux);
@@ -114,33 +86,48 @@
 		
 	}
 	else{//hay numero impar de publicaciones y es el ultimo(muestro solo una tarjeta)
-		//print_r("Indice: ".$indice."<br>");
-		$fila = $valores[$indice-2];
-		$titulo="Primera dominada";
+		$aux=$trozos[1];
+		$fila = $valores[$indice-3];
+		$titulo=$fila['TITULO'];
 		$descripcion = $fila['CONTENIDO'];
 		$autor = $fila['AUTOR'];
 		$fecha = $fila['FECHA_PUBLICACION'];
 
-		$aux=$trozos[1];
 		$aux=str_replace("##resena_titulo1##", $titulo, $aux);
 		$aux=str_replace("##resena_texto1##", $descripcion, $aux);
 		$aux=str_replace("##resena_autor1##", $autor, $aux);
 		$aux=str_replace("##resena_fecha1##", $fecha, $aux);
+		
+		$fila = $valores[$indice-2];
+		$titulo2=$fila['TITULO'];
+		$descripcion2 = $fila['CONTENIDO'];
+		$autor2 = $fila['AUTOR'];
+		$fecha2 = $fila['FECHA_PUBLICACION'];
+
+		$aux=str_replace("##resena_titulo2##", $titulo2, $aux);
+		$aux=str_replace("##resena_texto2##", $descripcion2, $aux);
+		$aux=str_replace("##resena_autor2##", $autor2, $aux);
+		$aux=str_replace("##resena_fecha2##", $fecha2, $aux);
 		$carta.=$aux;
+		
+		
 	}
-	
-	
 
 	$paginacion="";
+	$aux=$trozos[2];
+	$aux=str_replace("##pagina_anterior##", $boton_anterior, $aux);
+	$paginacion.=$aux;
 	for ($i=0; $i < $numero_pagina; $i++) { 
-		$aux=$$aux=$trozos[2];
+		$aux=$trozos[3];
 		$aux=str_replace("##numero_pagina##", $i+1, $aux);
 		$paginacion.=$aux;
 	}
+	$aux=$trozos[4];
+	$aux=str_replace("##pagina_siguiente##", $boton_siguiente, $aux);
+	$paginacion.=$aux;
 
-	
 
-	echo $trozos[0].$carta.$paginacion.$trozos[3];
+	echo $trozos[0].$carta.$paginacion.$trozos[5];
 
 
 	
