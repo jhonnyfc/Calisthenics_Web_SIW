9fe0470e5afa42585c0512e8b00e97f2982b6391
Daniel <email@example.com>
2020-04-03 10:57:34 +0200

    dsni

--- final/modelo.php
+++ final/modelo.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/vendor/autoload.php';
+	//require 'Constantes.php';
+
+	
 	function conexionbasedatos() {
 		$conexion = mysqli_connect("localhost", "root", "", "grupo33");
 
 		return $conexion;
 	}
 
-	/**
-	Funcion encargada de recoger la informacion de la tabla final_publicacion
-	Manda
-		resultado --> Si se ha realizado la consulta correctamente
-		-1 --> Si existe algun problema con la base de datos
-	**/
 	function mdatospublicaciones(){
 		$conexion = conexionbasedatos();
 
@@ -23,66 +24,51 @@
 		}
 	}
 
-	/**
-	Funcion encargada de recoger la informacion del formulario de contacto
-	Manda
-		 1--> Si se ha mandado el correo correctamente
-		-1 --> Si existe algun problema 
-	**/
 	function mdatoscorreo(){
-
+		
 		$destino = "danieldbg1calisteniaweb@gmail.com";
 		$nombre = $_POST["nombre"];
 		$email = $_POST["email"];
 		$telefono = $_POST["telefono"];
 		$mensaje = $_POST["mensaje"];
-		//direcciones que recibirán copia oculta (administrador)
-		//$headers .= "Bcc: pepe@pepe.com,juan@juan.com\r\n";
 
-		//Validar informacion del formulario (mejor con javascript)
-		
-		if (!preg_match("/^[a-zA-Z]*$/",$nombre)) {
-			return -2;
-		}
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			return -3;
-		}
-		if (!filter_var($telefono, FILTER_VALIDATE_INT)) {
-			return -4;
-		}
-		if (!preg_match("/^[a-zA-Z]*$/",$mensaje)) {
-			return -5;
-		}
-		
+		$mail = new PHPMailer(true);
 
-		$contenido = "Nombre: ".$nombre."\nCorreo: ".$email."\nTelefono: ".$telefono."\nMensaje: ".$mensaje;
+		//try {
+		    $mail->SMTPDebug = 2;
+		    $mail->isSMTP();
 
-		if (mail($destino, "CalisteniaWeb", $contenido/*$headers*/)){
-			return 1;
-		}
-		else{
-			return -1;
-		}
+		    $mail->Host = 'smtp.gmail.com';
+		    $mail->SMTPAuth = true;
+
+		    $mail->Username = 'danieldbg1Calisteniaweb@gmail.com';
+		    $mail->Password = 'danidbg1CalisteniaWeb';
+
+		    $mail->SMTPSecure = 'tls';
+		    $mail->Port = 587;
+
+		    ## MENSAJE A ENVIAR
+
+		    $mail->setFrom('danieldbg1Calisteniaweb@gmail.com');
+		    $mail->addAddress('danieldbg1Calisteniaweb@gmail.com');
+
+		    $mail->isHTML(true);
+		    $mail->Subject = 'CalisteniaWeb';
+		    $mail->Body = "Buenos dias, <br>
+		    				soy '$nombre' y este es mi problema:<br>
+		    				'$mensaje'<br><br>
+		    				Informacion de contacto:<br>
+		    				Telefono:'$telefono'<br>
+		    				Correo: '$email'";
+
+		    $mail->send($mail);
+		    return 1;
+
+		//} catch (Exception $exception) {
+		  //  echo 'Algo salio mal', $exception->getMessage();
+		//}
 	}
-
-	/**
-	Funcion encargada de recoger la informacion de la tabla final_ejercicio
-	Manda
-		resultado --> Si se ha realizado la consulta correctamente
-		-1 --> Si existe algun problema con la base de datos
-	**/
-	function mdatosejercicios(){
-		$conexion = conexionbasedatos();
-
-		$consulta = "select * from final_ejercicios";
-
-		if ($resultado = $conexion->query($consulta)) {
-			return $resultado;
-		} else {
-			return -1;
-		}
-
-	}
+		
 
 
 
